23554aa4d55e8e67f006396c02e31618a932c94b
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-24 17:58:15 -0600

    06

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,16 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST /api/products', () => {
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name : "Mouse - Testing",
@@ -13,6 +23,6 @@ describe('POST /api/products', () => {
 
         expect(response.status).not.toBe(404)
         expect(response.status).not.toBe(200)
-        expect(response.body).not.toHaveProperty('error')
+        expect(response.body).not.toHaveProperty('errors')
     })
 })
