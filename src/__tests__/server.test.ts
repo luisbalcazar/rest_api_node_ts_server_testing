d25a18f47040ad2e0e6ce86d32d20c098aba3ca1
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-25 16:45:30 -0600

    14

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
-import server from '../server'
+import server, { connectDB } from '../server'
+import db from '../config/db'
+
 
 describe('GET /api', () => {
     it('should send back a json response', async () => {
@@ -11,5 +13,21 @@ describe('GET /api', () => {
 
         expect(res.status).not.toBe(404)
         expect(res.body.msg).not.toBe('desde api')
+    })
+})
+
+jest.mock('../config/db')
+
+describe('connectDB', () => {
+    it('should handle database connection error', async () => {
+        jest.spyOn(db, 'authenticate')
+            .mockRejectedValueOnce(new Error('Hubo un error al conectar a la BD'))
+        const consoleSpy = jest.spyOn(console, 'log')
+
+        await connectDB()
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            expect.stringContaining('Hubo un error al conectar a la BD')
+        )
     })
 })
