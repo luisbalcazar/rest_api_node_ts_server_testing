a249a40b4a15f0f0c7eb90a53f5f47af07078e71
Luis Balcazar <luisbalcares17@gmail.com>
2026-02-17 00:21:57 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,49 @@
-import express from 'express' 
-import colors from 'colors'
-import router  from './router'
-import db from './config/db'
+import express from "express";
+import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
+import router from "./router";
+import db from "./config/db";
 
 // Conectar a base de datos
 export async function connectDB() {
-    try {
-        await db.authenticate()
-        db.sync()
-        // console.log( colors.blue( 'Conexión exitosa a la BD'))
-    } catch (error) {
-        // console.log(error)
-        console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
-    }
+  try {
+    await db.authenticate();
+    db.sync();
+    // console.log( colors.blue( 'Conexión exitosa a la BD'))
+  } catch (error) {
+    console.log(colors.red.bold("Hubo un error al conectar a la BD"));
+  }
 }
-connectDB()
+connectDB();
 
 // Instancia de express
-const server = express()
+const server = express();
+
+//Habilitamos los Cors
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === process.env.CLIENT_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 
 // Leer datos de formularios
-server.use(express.json())
+server.use(express.json());
 
-server.use('/api/products', router)
+//Logger
+server.use(morgan("common"));
 
-server.get('/api', (req, res) => {
-    res.json({msg: 'Desde API'})
-})
+//Config rutas
+server.use("/api/products", router);
 
-export default server
+/* server.get("/api", (req, res) => {
+  res.json({ msg: "Desde API" });
+}); */
+
+export default server;
